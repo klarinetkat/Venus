30d295bc2244baaaa56ab3e37e6f5e29e2e62176
Slessler123 <42000409+Slessler123@users.noreply.github.com>
2018-08-15 15:31:53 -0400

    Add files via upload

---
+++ venus.js
@@ -0,0 +1,60 @@
+y = 0
+list = [0,0,0,0,0];
+function addpoints(index){
+  list[index] = list[index] + 1;
+  y = y + 1
+  if (y == 5){
+    calculate(list)
+  }
+}
+
+function calculate(list){;
+  z = Math.max(...list);
+  if (countInArray(list, z) > 1){
+    tie(z,list)
+  }
+  else{
+    y = list.indexOf(z);
+    if (y == 0){
+      window.location = "tomboy.html";
+    }
+    if (y == 1){
+      window.location = "stylepage2.html";
+    }
+    if (y == 2){
+      window.location = "stylepage3.html";
+    }
+    if (y == 3){
+      window.location = "stylepage4.html";
+    }
+    if (y == 4){
+      window.location = "stylepage5.html";
+    }
+  }
+}
+
+function tie(max,list){
+  for (i = -1; i < 5; i++){
+    if (list[i] == max){
+      if (i == 0){
+        /*show image a in question*/;
+      }
+      if (i == 1){
+        /*show image b in question*/;
+      }
+      if (i == 2){
+        /*show image c in question*/;
+      }
+      if (i == 3){
+        /*show image d in question*/;
+      }
+      if (i == 4){
+        /*show image e in question*/;
+      }
+    }
+  }
+}
+
+function countInArray(array, value) {
+  return array.reduce((n, x) => n + (x === value), 0);
+}
