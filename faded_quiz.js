772531b0ec5c20fd37955b94de35177afb0d02c0
klarinetkat <42000416+klarinetkat@users.noreply.github.com>
2018-08-17 11:15:42 -0400

    Add files via upload

---
+++ faded_quiz.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#q1").click(function(){
+      $(this).fadeOut("fast");
+      $("#q2").fadeIn("fast");
+    });
+
+    $("#q2").click(function(){
+      $(this).fadeOut("fast");
+      $("#q3").fadeIn("fast");
+    });
+
+    $("#q3").click(function(){
+      $(this).fadeOut("fast");
+      $("#q4").fadeIn("fast");
+    });
+
+    $("#q4").click(function(){
+      $(this).fadeOut("fast");
+      $("#q5").fadeIn("fast");
+    });
+
+    $("#q5").click(function(){
+      $(this).fadeOut("fast");
+      $("#finish").fadeIn("fast");
+    });
+});
